cc327cdbe738e68b8176b5be4ccf55f1fdfd5538
Maxim Kashchenko <mkaschenko@me.com>
2016-03-05 21:37:16 +0700

    Add mkaschenko/dash-at-point
    Populates input by word under the point.
    Docset may be specified as the second word of input.

--- .emacs.d/elisp/init-misc.el
+++ .emacs.d/elisp/init-misc.el
@@ -1,4 +1,5 @@
 (require 'misc)
+(require 'dash-at-point)
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
@@ -11,9 +12,15 @@
   (let ((inhibit-read-only t))
     (erase-buffer)))
 
+(defun mkaschenko/dash-at-point ()
+  (interactive)
+  (let ((thing (read-string "dash: " (thing-at-point 'symbol))))
+    (apply 'dash-at-point-run-search (split-string thing))))
+
 (global-set-key (kbd "M-Z") 'zap-up-to-char)
 (global-set-key (kbd "C-j") 'delete-indentation)
 (global-set-key (kbd "C-c r") 'query-replace)
 (global-set-key (kbd "C-x P") 'previous-buffer)
+(global-set-key (kbd "C-c D") 'mkaschenko/dash-at-point)
 
 (provide 'init-misc)
