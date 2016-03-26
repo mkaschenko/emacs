92d121cd4b6eeace30ceac636b2c383aad1a42d3
Maxim Kashchenko <mkaschenko@me.com>
2016-03-26 21:59:47 +0700

    Use defvar instead of setq

--- .emacs.d/elisp/init-programming-mode-hooks.el
+++ .emacs.d/elisp/init-programming-mode-hooks.el
@@ -1,8 +1,8 @@
-(setq mkaschenko/lisp-programming-mode-hooks
-      '(emacs-lisp-mode-hook clojure-mode-hook))
+(defvar mkaschenko/lisp-programming-mode-hooks
+  '(emacs-lisp-mode-hook clojure-mode-hook))
 
-(setq mkaschenko/programming-mode-hooks
-      (append mkaschenko/lisp-programming-mode-hooks
-	      '(ruby-mode-hook haml-mode-hook slim-mode-hook web-mode-hook)))
+(defvar mkaschenko/programming-mode-hooks
+  (append mkaschenko/lisp-programming-mode-hooks
+	  '(ruby-mode-hook haml-mode-hook slim-mode-hook web-mode-hook)))
 
 (provide 'init-programming-mode-hooks)
