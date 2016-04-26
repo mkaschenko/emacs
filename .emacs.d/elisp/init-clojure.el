d95c6f08cba4c36cc19137fbfa59d85e435072b1
Maxim Kashchenko <mkaschenko@me.com>
2016-04-26 20:46:51 +0700

    Replace inf-clojure by cider

--- .emacs.d/elisp/init-clojure.el
+++ .emacs.d/elisp/init-clojure.el
@@ -1,5 +1,3 @@
 (setq clojure-align-forms-automatically t)
 
-(add-hook 'clojure-mode-hook 'inf-clojure-minor-mode)
-
 (provide 'init-clojure)
--- .emacs.d/elisp/init-eldoc.el
+++ .emacs.d/elisp/init-eldoc.el
@@ -1,5 +1,6 @@
-(add-hook 'inf-clojure-mode-hook 'eldoc-mode)
 (dolist (hook mkaschenko/lisp-programming-mode-hooks)
   (add-hook hook 'eldoc-mode))
+
+(add-hook 'cider-repl-mode-hook 'eldoc-mode)
 
 (provide 'init-eldoc)
