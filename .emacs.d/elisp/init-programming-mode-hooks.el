be71a5ebdbe25bce0545633e79b7b038165ce8a9
Maxim Kashchenko <mkaschenko@me.com>
2016-05-01 19:44:58 +0700

    Enable smartparens for non-lisp programming modes only

--- .emacs.d/elisp/init-programming-mode-hooks.el
+++ .emacs.d/elisp/init-programming-mode-hooks.el
@@ -1,8 +1,11 @@
 (defvar mkaschenko/lisp-programming-mode-hooks
   '(emacs-lisp-mode-hook clojure-mode-hook))
 
+(defvar mkaschenko/non-lisp-programming-mode-hooks
+  '(ruby-mode-hook haml-mode-hook slim-mode-hook web-mode-hook))
+
 (defvar mkaschenko/programming-mode-hooks
   (append mkaschenko/lisp-programming-mode-hooks
-	  '(ruby-mode-hook haml-mode-hook slim-mode-hook web-mode-hook)))
+	  mkaschenko/non-lisp-programming-mode-hooks))
 
 (provide 'init-programming-mode-hooks)
--- .emacs.d/elisp/init-smartparens.el
+++ .emacs.d/elisp/init-smartparens.el
@@ -1,15 +1,8 @@
-;; TODO: paredit vs smartparens
-;;       smartparens does not have paredit-wrap-round
-;;       http://mumble.net/%7Ecampbell/emacs/paredit.html
-;;       https://github.com/Fuco1/smartparens
 (require 'smartparens-config)
 
 (setq sp-autoescape-string-quote nil)
 
-(smartparens-global-mode t)
-
-(dolist (hook mkaschenko/lisp-programming-mode-hooks)
-  (add-hook hook 'show-smartparens-mode)
-  (add-hook hook 'smartparens-strict-mode))
+(dolist (hook mkaschenko/non-lisp-programming-mode-hooks)
+  (add-hook hook 'turn-on-smartparens-mode))
 
 (provide 'init-smartparens)
