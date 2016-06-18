594a8f834057ad4472e04c0b9192d906ab0fd2ff
Maxim Kashchenko <mkaschenko@me.com>
2016-06-18 14:03:03 +0700

    Enable clj-refactoring-mode for clojure-mode

--- .emacs.d/elisp/init-lisps.el
+++ .emacs.d/elisp/init-lisps.el
@@ -5,6 +5,12 @@
   (add-hook hook 'eldoc-mode)
   (add-hook hook 'show-smartparens-mode))
 
+(defun clj-refactor-mode-setup ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+(add-hook 'clojure-mode-hook 'clj-refactor-mode-setup)
+
 (require 'cider)
 (define-key cider-mode-map (kbd "C-M-y") 'flyspell-auto-correct-previous-word)
 
