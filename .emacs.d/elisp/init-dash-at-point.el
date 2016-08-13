435e0798c35da98c4dc4ab3fe6b9c92c3dbbfac6
Maxim Kashchenko <mkaschenko@me.com>
2016-08-13 10:49:12 +0700

    Rebind mkaschenko/dash-at-point to C-c d

--- .emacs.d/elisp/init-dash-at-point.el
+++ .emacs.d/elisp/init-dash-at-point.el
@@ -5,6 +5,6 @@
   (let ((thing (read-string "dash: " (thing-at-point 'symbol))))
     (apply 'dash-at-point-run-search (split-string thing))))
 
-(global-set-key (kbd "C-c D") 'mkaschenko/dash-at-point)
+(global-set-key (kbd "C-c d") 'mkaschenko/dash-at-point)
 
 (provide 'init-dash-at-point)
