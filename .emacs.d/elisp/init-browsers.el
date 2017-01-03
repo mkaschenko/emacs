99ad38f9f5f4c47b08f050bcf6ff2d501eb3ea7c
Maxim Kashchenko <mkaschenko@me.com>
2017-01-03 19:57:25 +0700

    Add !c alias for !clojure to search with DuckDuckGo

--- .emacs.d/elisp/init-browsers.el
+++ .emacs.d/elisp/init-browsers.el
@@ -1,7 +1,12 @@
 (defun mkaschenko/duckduckgo-search (query)
   (interactive "sDuckDuckGo: ")
   (browse-url
-   (concat "https://duckduckgo.com/?q=" (replace-regexp-in-string " " "+" query))))
+   (concat "https://duckduckgo.com/?q="
+	   (replace-regexp-in-string " " "+" (let ((bang (substring query 0 2))
+						   (rest (substring query 2)))
+					       (if (string= bang "!c")
+						   (concat "!clojure" rest)
+						 query))))))
 
 (global-set-key (kbd "C-c w") 'eww)
 (global-set-key (kbd "C-c W") 'mkaschenko/duckduckgo-search)
