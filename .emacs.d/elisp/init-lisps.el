211db5477fa4c540d4df6228836af2e8531feac8
Maxim Kashchenko <mkaschenko@me.com>
2017-01-03 19:54:09 +0700

    Refactor init-lisps.el
    - extract mkaschenko/setup-lisp
    - rename clj-refactor-mode-setup to mkaschenko/setup-clj-refactor

--- .emacs.d/elisp/init-lisps.el
+++ .emacs.d/elisp/init-lisps.el
@@ -1,15 +1,19 @@
 (setq clojure-align-forms-automatically t)
 
-(dolist (hook mkaschenko/lisp-programming-mode-hooks)
-  (add-hook hook 'enable-paredit-mode)
-  (add-hook hook 'eldoc-mode)
-  (add-hook hook 'show-smartparens-mode))
+(defun mkaschenko/setup-lisp ()
+  (enable-paredit-mode)
+  (eldoc-mode)
+  (show-smartparens-mode))
 
-(defun clj-refactor-mode-setup ()
+(dolist (hook mkaschenko/lisp-programming-mode-hooks)
+  (add-hook hook 'mkaschenko/setup-lisp))
+
+(defun mkaschenko/setup-clj-refactor ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
-(add-hook 'clojure-mode-hook 'clj-refactor-mode-setup)
+
+(add-hook 'clojure-mode-hook 'mkaschenko/setup-clj-refactor)
 
 (setq cljr-warn-on-eval nil)
 
@@ -17,9 +21,7 @@
 (define-key cider-mode-map (kbd "C-M-y") 'flyspell-auto-correct-previous-word)
 (define-key cider-mode-map (kbd "C-c M") 'complete-symbol)
 
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'eldoc-mode)
-(add-hook 'cider-repl-mode-hook 'show-smartparens-mode)
+(add-hook 'cider-repl-mode-hook 'mkaschenko/setup-lisp)
 (add-hook 'cider-mode-hook 'cider-auto-test-mode)
 
 (setq cider-prompt-save-file-on-load 'always-save)
