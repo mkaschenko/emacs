019fade548c37f860fabfea92264d61270ff645f
Maxim Kashchenko <mkaschenko@me.com>
2017-02-18 20:10:13 +0700

    Remove TODO about ag

--- .emacs.d/elisp/init-ag.el
+++ .emacs.d/elisp/init-ag.el
@@ -1,7 +1,3 @@
-;; TODO: don't use the hook after the new release
-;;       https://github.com/Wilfred/ag.el/issues/99
 (setq ag-highlight-search t)
-
-(add-hook 'ag-mode-hook 'toggle-truncate-lines)
 
 (provide 'init-ag)
