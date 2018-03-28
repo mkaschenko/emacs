79add55df5ee726a296050af303d61b6e816ef32
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 20:09:28 +0700

    Setup org

--- .emacs.d/elisp/init-org.el
+++ .emacs.d/elisp/init-org.el
@@ -1,12 +1,10 @@
-(setq org-completion-use-ido t)
-(setq
- org-todo-keywords
- '((sequence "TODO" "STARTED" "|" "DONE"))
- org-todo-keyword-faces
- '(("TODO" . (:background "pink" :weight bold))
-   ("STARTED" . (:background "khaki" :weight bold))))
-
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
+
+(setq org-completion-use-ido t
+      org-todo-keywords      '((sequence "TODO" "STARTED" "|" "DONE"))
+      org-todo-keyword-faces '(("TODO" . (:background "pink" :weight bold))
+                               ("STARTED" . (:background "khaki" :weight bold))))
+
 (add-hook 'org-mode-hook 'org-indent-mode)
 
 (provide 'init-org)
