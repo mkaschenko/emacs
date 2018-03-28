6d0e4e70becabab265a350ed0fc5dcf69b767d67
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:18:46 +0700

    Setup Magit

--- .emacs.d/elisp/init-magit.el
+++ .emacs.d/elisp/init-magit.el
@@ -1,15 +1,16 @@
-(setq magit-completing-read-function 'magit-ido-completing-read)
-(setq magit-popup-show-common-commands nil)
-(setq magit-fetch-arguments '("--prune"))
-(setq magit-log-arguments '("--graph" "--decorate" "--follow" "-n256"))
-(setq magit-merge-arguments '("--ff-only"))
-(setq magit-rebase-arguments '("--autosquash" "--autostash"))
+(setq magit-completing-read-function   'magit-ido-completing-read
+      magit-fetch-arguments            '("--prune")
+      magit-gh-pulls-arguments         '("--open-new-in-browser")
+      magit-log-arguments              '("--graph" "--decorate" "--follow" "-n256")
+      magit-merge-arguments            '("--ff-only")
+      magit-popup-show-common-commands nil
+      magit-rebase-arguments           '("--autosquash" "--autostash"))
 
-(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c L") 'magit-find-file)
 (global-set-key (kbd "C-c b") 'magit-blame)
+(global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-c l") 'magit-log-buffer-file)
 
-(require 'magit-gh-pulls)
 (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 
 (provide 'init-magit)
