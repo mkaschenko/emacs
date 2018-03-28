b267c9df491ca78d29c655c55907a057c7adddf1
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 20:04:42 +0700

    Delete unnecessary files

--- .emacs.d/elisp/my-eshell-clear-buffer.el
+++
@@ -1,8 +0,0 @@
-(defun my-eshell-clear-buffer ()
-  "Clear the selected Eshell buffer."
-  (interactive)
-  (let ((inhibit-read-only t))
-    (erase-buffer)
-    (eshell-send-input)))
-
-(provide 'my-eshell-clear-buffer)
--- .emacs.d/elisp/my-previous-window.el
+++
@@ -1,6 +0,0 @@
-(defun my-previous-window (count)
-  "Select the previous window in cyclic ordering of windows."
-  (interactive "p")
-  (other-window (- count)))
-
-(provide 'my-previous-window)
