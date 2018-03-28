efc664895a8d8fed7f9be946d9be58d07acc996d
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:16:11 +0700

    Delete unnecessary init files

--- .emacs.d/elisp/init-aggressive-indent.el
+++
@@ -1,3 +0,0 @@
-(global-aggressive-indent-mode 1)
-
-(provide 'init-aggressive-indent)
--- .emacs.d/elisp/init-subword.el
+++
@@ -1,6 +0,0 @@
-(dolist (hook mkaschenko/programming-mode-hooks)
-  (add-hook hook 'subword-mode))
-
-(add-hook 'inf-ruby-mode-hook 'subword-mode)
-
-(provide 'init-subword)
