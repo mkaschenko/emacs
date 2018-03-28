fbe84b1014c9c4b8e0c757cc571fa2e0121a734a
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:16:50 +0700

    Setup winner mode

--- .emacs.d/elisp/init-winner.el
+++ .emacs.d/elisp/init-winner.el
@@ -1,8 +1,9 @@
-(winner-mode 1)
-
-(global-set-key (kbd "C-x p") 'winner-undo)
-(global-set-key (kbd "C-x O") 'winner-redo)
-
 (setq winner-boring-buffers nil)
+
+(global-set-key (kbd "C-x O") 'winner-redo)
+(global-set-key (kbd "C-x P") 'previous-buffer)
+(global-set-key (kbd "C-x p") 'winner-undo)
+
+(winner-mode +1)
 
 (provide 'init-winner)
