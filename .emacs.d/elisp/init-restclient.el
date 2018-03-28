0a644e31d04407b037da4daf51c822bd0bd5515a
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:33:55 +0700

    Setup restclient

--- .emacs.d/elisp/init-restclient.el
+++ .emacs.d/elisp/init-restclient.el
@@ -1,9 +1,9 @@
+(add-to-list 'auto-mode-alist '("\\.rc\\'" . restclient-mode))
+
 (defun mkaschenko/setup-restclient ()
-  (aggressive-indent-mode -1)
   (show-paren-mode +1)
   (smartparens-mode +1))
 
-(add-to-list 'auto-mode-alist '("\\.rc\\'" . restclient-mode))
 (add-hook 'restclient-mode-hook 'mkaschenko/setup-restclient)
 
 (provide 'init-restclient)
