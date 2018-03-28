cf4c8c1cce8d3cae05b47ccc34bbe9e6f2fe4e6c
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 20:08:56 +0700

    Setup multiple cursors

--- .emacs.d/elisp/init-multiple-cursors.el
+++ .emacs.d/elisp/init-multiple-cursors.el
@@ -1,5 +1,6 @@
 (global-set-key (kbd "C-c .") 'mc/mark-next-like-this-word)
-(global-set-key (kbd "C-c >") 'mc/mark-previous-word-like-this)
 (global-set-key (kbd "C-c /") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c <") 'mc/mark-previous-word-like-this)
+(global-set-key (kbd "C-c >") 'mc/mark-next-word-like-this)
 
 (provide 'init-multiple-cursors)
