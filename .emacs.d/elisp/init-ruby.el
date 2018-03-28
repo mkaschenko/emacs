accd30f76716b9c4e68d67b8db2fe07f223be844
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:18:10 +0700

    Setup ruby mode

--- .emacs.d/elisp/init-ruby.el
+++ .emacs.d/elisp/init-ruby.el
@@ -1,33 +1,31 @@
-(setq ruby-insert-encoding-magic-comment nil)
-
-(setq rspec-autosave-buffer t)
-
-(setq projectile-tags-command "ripper-tags -Re -f TAGS")
-
+(setq projectile-tags-command            "ripper-tags -Re -f TAGS"
+      rspec-autosave-buffer              t
+      ruby-insert-encoding-magic-comment nil)
 
 (defun mkaschenko/setup-ruby-keys ()
-  ;; (define-key ruby-mode-map (kbd "C-c C-i") 'inf-ruby)
-  ;; (define-key ruby-mode-map (kbd "C-c C-c") 'inf-ruby-console-auto)
-  (define-key ruby-mode-map (kbd "C-c C-c") 'rubocop-check-current-file)
-  (define-key ruby-mode-map (kbd "C-c C-v") 'rubocop-autocorrect-current-file)
-  ;; (define-key ruby-mode-map (kbd "C-c C-d") 'yari)
-  ;; (define-key ruby-mode-map (kbd "C-c C-t") 'coverage-mode)
-  )
+  (define-key ruby-mode-map (kbd "C-c C-c") 'inf-ruby-console-auto)
+  (define-key ruby-mode-map (kbd "C-c C-i") 'inf-ruby)
+  (define-key ruby-mode-map (kbd "C-c C-t") 'coverage-mode)
+  (define-key ruby-mode-map (kbd "C-c C-v") 'rubocop-check-current-file)
+  (define-key ruby-mode-map (kbd "C-c C-y") 'yari)
+  (define-key ruby-mode-map (kbd "C-c V")   'rubocop-autocorrect-current-file))
 
 (defun mkaschenko/setup-ruby ()
-  (show-paren-mode +1)
+  (aggressive-indent-mode +1)
+  (linum-mode +1)
   (robe-mode +1)
+  (show-paren-mode +1)
+  (subword-mode +1)
   (mkaschenko/setup-ruby-keys))
 
 (defun mkaschenko/setup-inf-ruby ()
-  (aggressive-indent-mode -1)
   (show-paren-mode +1)
-  (smartparens-mode +1))
-
+  (smartparens-mode +1)
+  (subword-mode +1))
 
 (add-hook 'ruby-mode-hook 'mkaschenko/setup-ruby)
 
-(add-hook 'after-init-hook 'inf-ruby-switch-setup)
 (add-hook 'inf-ruby-mode-hook 'mkaschenko/setup-inf-ruby)
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
 
 (provide 'init-ruby)
