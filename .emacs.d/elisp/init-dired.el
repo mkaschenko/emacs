a73a23a6990094e1597acfa8e05a2149c59e76f9
Maxim Kashchenko <mkaschenko@me.com>
2018-03-28 17:19:04 +0700

    Setup Dired

--- .emacs.d/elisp/init-dired.el
+++ .emacs.d/elisp/init-dired.el
@@ -1,7 +1,10 @@
-(setq dired-dwim-target t)
-
 (require 'dired-details)
+
 (setq-default dired-details-hidden-string "--- ")
+
+(setq dired-dwim-target  t
+      dired-use-ls-dired nil)
+
 (dired-details-install)
 
 (provide 'init-dired)
