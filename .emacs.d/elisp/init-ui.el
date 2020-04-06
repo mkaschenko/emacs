58a9063915e854c6672e3f500b00f09074a56db5
Maxim Kashchenko <mkaschenko@me.com>
2020-04-06 17:59:04 +0700

    Indentation

--- .emacs.d/elisp/init-ui.el
+++ .emacs.d/elisp/init-ui.el
@@ -1,11 +1,10 @@
-(menu-bar-mode -1)
-
-(setq-default mode-line-format
-	      '(" "
-		mode-line-modified " "
-		mode-line-buffer-identification "       "
-		mode-line-position "       "
-		"%[" mode-name "%]"))
+(setq-default mode-line-format '(" "
+                                 mode-line-modified " "
+                                 mode-line-buffer-identification "       "
+                                 mode-line-position "       "
+                                 "%[" mode-name "%]"))
 (setq column-number-mode t)
+
+(menu-bar-mode -1)
 
 (provide 'init-ui)
