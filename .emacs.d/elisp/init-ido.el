59270e14a9ad1a6bbeeb7f3fd5f45fc7f7c21306
Maxim Kashchenko <mkaschenko@me.com>
2020-04-06 18:05:14 +0700

    Indentation

--- .emacs.d/elisp/init-ido.el
+++ .emacs.d/elisp/init-ido.el
@@ -1,11 +1,11 @@
-(ido-mode t)
+(flx-ido-mode t)
 (ido-everywhere t)
+(ido-mode t)
 (ido-ubiquitous-mode t)
 (ido-yes-or-no-mode t)
-(flx-ido-mode t)
 
 (global-set-key (kbd "C-c m") 'idomenu)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "M-X")   'smex-major-mode-commands)
+(global-set-key (kbd "M-x")   'smex)
 
 (provide 'init-ido)
