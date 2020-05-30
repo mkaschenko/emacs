bd1bd9293041dc815386b9675d19636309fb647e
Maxim Kashchenko <mkaschenko@me.com>
2020-05-30 15:23:08 +0700

    Toggle light and dark themes at 6 am and 8 pm correspondingly

--- .emacs.d/elisp/init-solarized-theme.el
+++ .emacs.d/elisp/init-solarized-theme.el
@@ -1,5 +1,30 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized/")
 
-(load-theme 'solarized t)
+(defun mkaschenko/solarized-light ()
+  (interactive)
+  (setq frame-background-mode 'light)
+  (load-theme 'solarized t nil))
+
+(defun mkaschenko/solarized-dark ()
+  (interactive)
+  (setq frame-background-mode 'dark)
+  (load-theme 'solarized t nil))
+
+(defun mkaschenko/hours ()
+  (string-to-number (format-time-string "%H" (current-time))))
+
+(defun mkaschenko/daytime? (hours)
+  (and (>= hours 6) (< hours 20)))
+
+(defun mkaschenko/solarized ()
+  (interactive)
+  (if (mkaschenko/daytime? (mkaschenko/hours))
+      (mkaschenko/solarized-light)
+    (mkaschenko/solarized-dark)))
+
+(mkaschenko/solarized)
+
+(run-at-time "06:00" (* 24 3600) #'mkaschenko/solarized)
+(run-at-time "20:00" (* 24 3600) #'mkaschenko/solarized)
 
 (provide 'init-solarized-theme)
