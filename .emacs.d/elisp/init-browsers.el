7d4dc3a6d211faf93993b18f0dcf2b65b80a9a1a
Maxim Kashchenko <mkaschenko@me.com>
2020-05-30 15:25:50 +0700

    Update mkaschenko/duckduckgo-search

--- .emacs.d/elisp/init-browsers.el
+++ .emacs.d/elisp/init-browsers.el
@@ -3,12 +3,7 @@
 (defun mkaschenko/duckduckgo-search (query)
   (interactive "sDuckDuckGo: ")
   (browse-url
-   (concat "https://duckduckgo.com/?q="
-	   (replace-regexp-in-string " " "+" (let ((bang (substring query 0 2))
-						   (rest (substring query 2)))
-					       (if (string= bang "!c")
-						   (concat "!clojure" rest)
-						 query))))))
+   (concat "https://duckduckgo.com/?q=" (replace-regexp-in-string " " "+" query))))
 
 (global-set-key (kbd "C-c W") 'browse-url)
 (global-set-key (kbd "C-c w") 'mkaschenko/duckduckgo-search)
