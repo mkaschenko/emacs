b3ccc4841725edcf132e0772b209c5caa44a2e1b
Maxim Kashchenko <mkaschenko@me.com>
2021-01-09 14:30:44 +0700

    Require dependencies within init-ido

--- .emacs.d/elisp/init-ido.el
+++ .emacs.d/elisp/init-ido.el
@@ -1,8 +1,12 @@
-(flx-ido-mode t)
-(ido-everywhere t)
+(require 'ido-completing-read+)
+(require 'ido-yes-or-no)
+(require 'flx-ido)
+
 (ido-mode t)
+(ido-everywhere t)
 (ido-ubiquitous-mode t)
 (ido-yes-or-no-mode t)
+(flx-ido-mode t)
 
 (global-set-key (kbd "C-c m") 'idomenu)
 (global-set-key (kbd "M-X")   'smex-major-mode-commands)
