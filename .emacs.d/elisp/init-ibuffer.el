dda033d7e1e5f535d0490b8f80a02d4e9f2e9d21
Maxim Kashchenko <mkaschenko@me.com>
2021-01-09 14:29:37 +0700

    Setup ibuffer
    - Set default sorting mode
    - Add mkaschenko/ibuffer-jump-to-last-buffer

--- .emacs.d/elisp/init-ibuffer.el
+++ .emacs.d/elisp/init-ibuffer.el
@@ -1,3 +1,11 @@
+(setq ibuffer-default-sorting-mode 'filename/process)
+
+(defun mkaschenko/ibuffer-jump-to-last-buffer ()
+  (ibuffer-jump-to-buffer (buffer-name (cadr (buffer-list)))))
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(add-hook 'ibuffer-hook 'mkaschenko/ibuffer-jump-to-last-buffer)
+(add-hook 'ibuffer-mode-hook 'ibuffer-auto-mode)
 
 (provide 'init-ibuffer)
