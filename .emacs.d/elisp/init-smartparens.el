80df636b55800728bccae59dc5b887a3261a227b
Maxim Kashchenko <mkaschenko@me.com>
2021-01-09 14:58:55 +0700

    Setup keys for smartparens functions

--- .emacs.d/elisp/init-smartparens.el
+++ .emacs.d/elisp/init-smartparens.el
@@ -4,7 +4,11 @@
 (setq sp-autoescape-string-quote nil)
 
 (defun mkaschenko/setup-smartparens-keys ()
-  (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp))
+  (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp)
+  (define-key smartparens-mode-map (kbd "C-M-s") 'sp-splice-sexp)
+  (define-key smartparens-mode-map (kbd "C-M-r") 'sp-raise-sexp)
+  (define-key smartparens-mode-map (kbd "C-c 9") 'sp-backward-barf-sexp)
+  (define-key smartparens-mode-map (kbd "C-c 0") 'sp-forward-slurp-sexp))
 
 (add-hook 'smartparens-mode-hook 'mkaschenko/setup-smartparens-keys)
 
