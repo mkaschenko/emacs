1433776b1b54e473b5fcc2ac0ca09aa7fde05485
Maxim Kashchenko <mkaschenko@me.com>
2021-01-09 14:36:49 +0700

    Add mkaschenko/setup-js

--- .emacs.d/elisp/init-javascript.el
+++ .emacs.d/elisp/init-javascript.el
@@ -1,3 +1,13 @@
 (setq js-indent-level 2)
 
+(defun mkaschenko/setup-js ()
+  (aggressive-indent-mode +1)
+  (flyspell-prog-mode)
+  (linum-mode +1)
+  (show-paren-mode +1)
+  (smartparens-mode +1)
+  (subword-mode +1))
+
+(add-hook 'js-mode-hook 'mkaschenko/setup-js)
+
 (provide 'init-javascript)
