f26aaeb46954de6fd3958b76dadcad94c30dadb3
Maxim Kashchenko <mkaschenko@me.com>
2021-03-03 19:37:25 +0700

    Update Dired buffers output
    - Display file sizes in Kb
    - Hide .git, .DS_Store

--- .emacs.d/elisp/init-dired.el
+++ .emacs.d/elisp/init-dired.el
@@ -1,11 +1,21 @@
 (require 'dired-details)
+(require 'ls-lisp)
 
 (setq-default dired-details-hidden-string "--- ")
 
-(setq dired-dwim-target        t
-      dired-auto-revert-buffer t
-      dired-use-ls-dired       nil)
+(setq dired-auto-revert-buffer t
+      dired-dwim-target        t
+      dired-listing-switches   "-alh"
+      dired-use-ls-dired       nil
+      ls-lisp-dirs-first       t
+      ls-lisp-use-insert-directory-program nil)
+
+(defun mkaschenko/dired-hide ()
+  (dired-mark-files-regexp "^\\.git$\\|^\\.DS_Store$")
+  (dired-do-kill-lines)
+  (message nil))
 
 (dired-details-install)
+(add-hook 'dired-after-readin-hook 'mkaschenko/dired-hide)
 
 (provide 'init-dired)
