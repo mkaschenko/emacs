3220641473e809be10bd5d469ba7d8154b95fdb3
Maxim Kashchenko <mkaschenko@me.com>
2021-08-06 17:14:51 +0700

    Don't create missing test files

--- .emacs.d/elisp/init-projectile.el
+++ .emacs.d/elisp/init-projectile.el
@@ -1,4 +1,4 @@
-(setq projectile-create-missing-test-files t)
+(setq projectile-create-missing-test-files nil)
 
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
