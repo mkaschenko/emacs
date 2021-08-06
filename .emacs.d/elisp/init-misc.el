5499d0f3af712add998dd971fe16d7a7ce1255ac
Maxim Kashchenko <mkaschenko@me.com>
2021-08-06 17:47:49 +0700

    Bind zap-up-to-char to M-z

--- .emacs.d/elisp/init-misc.el
+++ .emacs.d/elisp/init-misc.el
@@ -32,7 +32,7 @@
 (global-set-key (kbd "C-c ;") 'mkaschenko/toggle-comment-on-line)
 (global-set-key (kbd "C-c r") 'query-replace)
 (global-set-key (kbd "C-c C-j") 'delete-indentation)
-(global-set-key (kbd "M-Z") 'zap-up-to-char)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
 (global-set-key (kbd "M-+") 'mkaschenko/pbcopy-on-region)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
