2a1d33372a09b6ae79e4c009d0af944b460bde7e
Maxim Kashchenko <mkaschenko@me.com>
2021-08-06 17:13:49 +0700

    Bind re-wrap expression to C-c [

--- .emacs.d/elisp/init-smartparens.el
+++ .emacs.d/elisp/init-smartparens.el
@@ -8,7 +8,8 @@
   (define-key smartparens-mode-map (kbd "C-M-s") 'sp-splice-sexp)
   (define-key smartparens-mode-map (kbd "C-M-r") 'sp-raise-sexp)
   (define-key smartparens-mode-map (kbd "C-c 9") 'sp-backward-barf-sexp)
-  (define-key smartparens-mode-map (kbd "C-c 0") 'sp-forward-slurp-sexp))
+  (define-key smartparens-mode-map (kbd "C-c 0") 'sp-forward-slurp-sexp)
+  (define-key smartparens-mode-map (kbd "C-c [") 'sp-rewrap-sexp))
 
 (add-hook 'smartparens-mode-hook 'mkaschenko/setup-smartparens-keys)
 
