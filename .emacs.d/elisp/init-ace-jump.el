9b7950f4049c7079077f326322b8a7d7731ad029
Maxim Kashchenko <mkaschenko@me.com>
2021-08-07 17:06:41 +0700

    Configure ace jump mode

--- .emacs.d/elisp/init-ace-jump.el
+++ .emacs.d/elisp/init-ace-jump.el
@@ -1,12 +1,22 @@
+(require 'ace-jump-mode)
+(require 'magit)
 (require 'org)
 (require 'paredit)
 
-(setq ace-jump-mode-case-fold nil)
+(setq ace-jump-mode-case-fold    nil
+      ace-jump-mode-submode-list '(ace-jump-char-mode
+                                   ace-jump-line-mode
+                                   ace-jump-word-mode))
 
 (define-key lisp-interaction-mode-map (kbd "C-j") nil)
+
+(define-key magit-file-section-map (kbd "C-j") nil)
+(define-key magit-diff-section-base-map (kbd "C-j") nil)
+
 (define-key org-mode-map (kbd "C-j") nil)
+
 (define-key paredit-mode-map (kbd "C-j") nil)
 
-(global-set-key (kbd "C-j") 'ace-jump-char-mode)
+(global-set-key (kbd "C-j") 'ace-jump-mode)
 
 (provide 'init-ace-jump)
