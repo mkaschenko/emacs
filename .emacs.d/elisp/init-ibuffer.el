af30ed8d1d2e86213e7c3953b30ee23a7d816c4b
Maxim Kashchenko <mkaschenko@me.com>
2021-08-07 10:55:40 +0700

    Configure ibuffer mode
    - Use the default sorting mode (by recency)
    - Change width of columns
    - Remove 'Size' column

--- .emacs.d/elisp/init-ibuffer.el
+++ .emacs.d/elisp/init-ibuffer.el
@@ -1,11 +1,18 @@
-(setq ibuffer-default-sorting-mode 'filename/process)
-
-(defun mkaschenko/ibuffer-jump-to-last-buffer ()
-  (ibuffer-jump-to-buffer (buffer-name (cadr (buffer-list)))))
+(setq ibuffer-formats '((mark " "
+                              (name 35 35 :left :elide)
+                              "   "
+                              ;; (size 9 -1 :right)
+                              ;; " "
+                              (mode 18 18 :left :elide)
+                              "   "
+                              filename-and-process)
+                        (mark " "
+                              (name 16 -1)
+                              " "
+                              filename)))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-(add-hook 'ibuffer-hook 'mkaschenko/ibuffer-jump-to-last-buffer)
 (add-hook 'ibuffer-mode-hook 'ibuffer-auto-mode)
 
 (provide 'init-ibuffer)
