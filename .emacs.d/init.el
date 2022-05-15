89ed03dc3beb8417d60c80c076cd25cb69704e96
Maxim Kashchenko <mkaschenko@me.com>
2022-05-15 16:33:08 +0700

    .emacs -> .emacs.d/init.el
    https://www.gnu.org/software/emacs/manual/html_node/emacs/Init-File.html
    
    49.4 The Emacs Initialization File
    
    When Emacs is started, it normally tries to load a Lisp program from
    an initialization file, or init file for short. This file, if it
    exists, specifies how to initialize Emacs for you. Traditionally, file
    ~/.emacs is used as the init file, although Emacs also looks at
    ~/.emacs.el, ~/.emacs.d/init.el, ~/.config/emacs/init.el, or other
    locations. See How Emacs Finds Your Init File.
    
    You may find it convenient to have all your Emacs configuration in one
    directory, in which case you should use ~/.emacs.d/init.el or the
    XDG-compatible ~/.config/emacs/init.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,9 +3,9 @@
 (require 'init-ace-jump)
 (require 'init-ag)
 (require 'init-browsing)
-(require 'init-c++)
+;; (require 'init-c++)
 (require 'init-css)
-(require 'init-dash-search)
+;; (require 'init-dash-search)
 (require 'init-dictionary-at-point)
 (require 'init-dired)
 (require 'init-expand-region)
@@ -15,12 +15,12 @@
 (require 'init-ido)
 (require 'init-imenu)
 (require 'init-javascript)
-(require 'init-linum)
+;; (require 'init-linum)
 (require 'init-lisps)
 (require 'init-magit)
 (require 'init-misc)
 (require 'init-multiple-cursors)
-(require 'init-org)
+;; (require 'init-org)
 (require 'init-package)
 (require 'init-projectile)
 (require 'init-restclient)
@@ -28,24 +28,22 @@
 (require 'init-shell-script)
 (require 'init-shells)
 (require 'init-smartparens)
-(require 'init-solarized-theme)
 (require 'init-sql)
 (require 'init-text)
+(require 'init-theme)
 (require 'init-ui)
 (require 'init-winner)
-
 ;; TODO: (require 'init-emmet) https://github.com/smihica/emmet-mode
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(smartparens dash markdown-mode robe rubocop ace-jump-mode inf-ruby emmet-mode magit magit-popup ox-gfm marshal rspec-mode yari yaml-mode smex restclient projectile idomenu ido-yes-or-no ido-ubiquitous gitignore-mode gitconfig-mode gitattributes-mode git-link fuzzy flx-ido expand-region dired-details dash-at-point csv-mode clojure-cheatsheet clj-refactor buffer-move aggressive-indent ag)))
+   '(inf-clojure yari yaml-mode smex smartparens slim-mode rubocop rspec-mode robe restclient request projectile ox-gfm marshal magit-popup magit idomenu ido-yes-or-no ido-ubiquitous gitignore-mode gitconfig-mode gitattributes-mode git-link fuzzy flx-ido expand-region emmet-mode dired-details csv-mode clojure-cheatsheet clj-refactor buffer-move aggressive-indent ag ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-diff-added-highlight ((t (:background "#e4e4e4" :foreground "#5f8700")))))
--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf 'Syncing...'
-cp ~/.emacs .
+cp ~/.emacs.d/init.el .emacs.d/
 cp -r ~/.emacs.d/elisp/ .emacs.d/elisp/
 rm -rf .emacs.d/elisp/.git
 printf 'done\n'
