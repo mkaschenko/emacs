1d0879450612d95d7728b7efd8bdf5cd7a2a73e8
Maxim Kashchenko <mkaschenko@me.com>
2022-07-16 15:57:37 +0700

    Prefer built-in dired-hide-details-mode over a third party

--- .emacs.d/elisp/init-dired.el
+++ .emacs.d/elisp/init-dired.el
@@ -1,8 +1,5 @@
-(require 'dired-details)
 (require 'dired-x)
 (require 'ls-lisp)
-
-(setq-default dired-details-hidden-string "--- ")
 
 (setq dired-auto-revert-buffer t
       dired-dwim-target t
@@ -16,9 +13,8 @@
   (dired-do-kill-lines)
   (message nil))
 
-(dired-details-install)
-
 (add-hook 'dired-after-readin-hook 'mkaschenko/dired-hide)
 (add-hook 'dired-mode-hook 'auto-revert-mode)
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 (provide 'init-dired)
