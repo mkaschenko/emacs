724024aaca772fe303fa08ef8e84249322dcf304
Maxim Kashchenko <mkaschenko@me.com>
2022-08-14 16:05:27 +0700

    Remove smex
    http://xahlee.info/emacs/emacs/emacs_icomplete_mode.html

--- .emacs.d/elisp/init-ido.el
+++ .emacs.d/elisp/init-ido.el
@@ -1,15 +1,11 @@
 (require 'flx-ido)
 (require 'ido-completing-read+)
 (require 'ido-yes-or-no)
-(require 'smex)
 
 (ido-mode t)
 (ido-everywhere t)
 (ido-ubiquitous-mode t)
 (ido-yes-or-no-mode t)
 (flx-ido-mode t)
-
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "M-x") 'smex)
 
 (provide 'init-ido)
