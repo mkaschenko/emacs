ade2a4279b27c6d717ffd6ff47e591f4035ae0be
Maxim Kashchenko <mkaschenko@me.com>
2022-09-06 12:13:31 +0700

    Bind some Magit keys to capitals

--- .emacs.d/elisp/init-magit.el
+++ .emacs.d/elisp/init-magit.el
@@ -1,10 +1,9 @@
 (setq magit-completing-read-function 'magit-ido-completing-read
       magit-popup-show-common-commands nil)
 
-(global-set-key (kbd "C-c b") 'magit-blame)
+(global-set-key (kbd "C-c B") 'magit-blame-addition)
+(global-set-key (kbd "C-c F") 'magit-find-file)
+(global-set-key (kbd "C-c L") 'magit-log-buffer-file)
 (global-set-key (kbd "C-c g") 'magit-status)
 
 (provide 'init-magit)
-
-;; (global-set-key (kbd "C-c F") 'magit-find-file)
-;; (global-set-key (kbd "C-c L") 'magit-log-buffer-file)
