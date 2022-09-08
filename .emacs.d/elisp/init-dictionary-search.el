74c70ecf26e29ea963cad91b32eb7362cae2c6ba
Maxim Kashchenko <mkaschenko@me.com>
2022-09-08 16:45:53 +0700

    mkaschenko/dictionary-search suggests a default input

--- .emacs.d/elisp/init-dictionary-search.el
+++ .emacs.d/elisp/init-dictionary-search.el
@@ -1,6 +1,6 @@
-(defun mkaschenko/dictionary-search (query)
-  (interactive "sDictionary: ")
-  (shell-command (concat "open dict://\"" query "\"")))
+(defun mkaschenko/dictionary-search ()
+  (interactive)
+  (shell-command (concat "open dict://\"" (read-string "Dictionary: " (thing-at-point 'symbol)) "\"")))
 
 (global-set-key (kbd "C-c d") 'mkaschenko/dictionary-search)
 
