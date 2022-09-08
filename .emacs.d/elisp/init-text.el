0a2fba3a8e4509dd524809f2bbcee7210eb8ab4b
Maxim Kashchenko <mkaschenko@me.com>
2022-09-08 21:04:41 +0700

    Disable visualisation of matching parens within text

--- .emacs.d/elisp/init-text.el
+++ .emacs.d/elisp/init-text.el
@@ -1,7 +1,7 @@
 (defun mkaschenko/setup-text-mode ()
   (auto-fill-mode +1)
   (flyspell-mode +1)
-  (show-paren-mode +1))
+  (show-paren-mode -1))
 
 (add-hook 'text-mode-hook 'mkaschenko/setup-text-mode)
 
