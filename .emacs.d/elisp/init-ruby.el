fe03c75cac95395ec3dedb6eff3edaf9b5eb941d
Maxim Kashchenko <mkaschenko@me.com>
2022-09-14 15:50:24 +0700

    Add yari to inf-ruby

--- .emacs.d/elisp/init-ruby.el
+++ .emacs.d/elisp/init-ruby.el
@@ -2,12 +2,6 @@
       rspec-use-spring-when-possible nil
       ruby-insert-encoding-magic-comment nil)
 
-(defun mkaschenko/setup-ruby-keys ()
-  (define-key ruby-mode-map (kbd "C-c C-v") 'rubocop-autocorrect-current-file)
-  (define-key ruby-mode-map (kbd "C-c y") 'yari))
-
-(add-hook 'ruby-mode-hook 'mkaschenko/setup-ruby-keys)
-
 (defun mkaschenko/setup-ruby ()
   (display-line-numbers-mode +1)
   (flyspell-prog-mode)
@@ -15,16 +9,25 @@
   (smartparens-mode +1)
   (subword-mode +1))
 
+(defun mkaschenko/setup-ruby-keys ()
+  (define-key ruby-mode-map (kbd "C-c C-v") 'rubocop-autocorrect-current-file)
+  (define-key ruby-mode-map (kbd "C-c y") 'yari))
+
 (add-hook 'ruby-mode-hook 'mkaschenko/setup-ruby)
+(add-hook 'ruby-mode-hook 'mkaschenko/setup-ruby-keys)
 
 (defun mkaschenko/setup-inf-ruby ()
   (show-paren-mode +1)
   (smartparens-mode +1)
   (subword-mode +1))
 
-(add-hook 'inf-ruby-mode-hook 'mkaschenko/setup-inf-ruby)
-(add-hook 'after-init-hook 'inf-ruby-switch-setup)
+(defun mkaschenko/setup-inf-ruby-keys ()
+  (define-key inf-ruby-mode-map (kbd "C-c y") 'yari))
 
+(add-hook 'inf-ruby-mode-hook 'mkaschenko/setup-inf-ruby)
+(add-hook 'inf-ruby-mode-hook 'mkaschenko/setup-inf-ruby-keys)
+
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
 (add-hook 'dired-mode-hook 'rspec-dired-mode)
 
 (provide 'init-ruby)
