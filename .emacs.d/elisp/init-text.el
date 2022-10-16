88d2ab275bed3bafd78bee1413881cd3cd7c880d
Maxim Kashchenko <mkaschenko@me.com>
2022-10-16 08:36:54 +0700

    Enable subword mode for text mode

--- .emacs.d/elisp/init-text.el
+++ .emacs.d/elisp/init-text.el
@@ -1,6 +1,7 @@
 (defun mkaschenko/setup-text-mode ()
   (flyspell-mode +1)
-  (show-paren-mode -1))
+  (show-paren-mode -1)
+  (subword-mode +1))
 
 (add-hook 'text-mode-hook 'mkaschenko/setup-text-mode)
 
