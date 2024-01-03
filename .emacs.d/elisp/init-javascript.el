7d0bc113cf7b94ccc6bb240ad7a4e0a0d026d75f
Maxim Kashchenko <mkaschenko@me.com>
2024-01-03 17:06:22 +0700

    Setup json-mode

--- .emacs.d/elisp/init-javascript.el
+++ .emacs.d/elisp/init-javascript.el
@@ -9,4 +9,13 @@
 
 (add-hook 'js-mode-hook 'mkaschenko/setup-js)
 
+(defun mkaschenko/setup-json ()
+  (display-line-numbers-mode +1)
+  (flyspell-prog-mode)
+  (show-paren-local-mode -1)
+  (smartparens-mode +1)
+  (subword-mode +1))
+
+(add-hook 'js-json-mode-hook 'mkaschenko/setup-json)
+
 (provide 'init-javascript)
